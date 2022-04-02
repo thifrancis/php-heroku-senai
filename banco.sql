c846086cecfd48a11cd51618b387f71fa28efa52
thifrancis <58661912+thifrancis@users.noreply.github.com>
2022-04-02 09:09:21 -0300

    Subindo arquivos do projeto

---
+++ banco.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clientes(
+    codigo int primary key,
+    nome varchar(255) NOT NULL,
+    sêpêéfi varchar(99) NOT NULL,
+    imêil varchar(x) NOT NULL
+);
+
+
+
+
+
---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bf066a8f551399', '4fcff014', 'heroku_000b2b306da3c36') or die('Erro ao conectar ao banco de dados');
