187f49accc79b980e506d1f1ad0a75ed534e9360
thifrancis <58661912+thifrancis@users.noreply.github.com>
2022-04-02 09:33:31 -0300

    correcao de caminho

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 
 		<nav class="navbar navbar-expand-lg navbar-dark bg-primary rounded">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="index-errado.php">::: NetChute :::</a>
+				<a class="navbar-brand" href="index.php">::: NetChute :::</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
